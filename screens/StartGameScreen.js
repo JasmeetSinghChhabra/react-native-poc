9936327554916a2902b63e937ce9275cc10744a2
JasmeetSinghChhabra <js.chhabra1994@gmail.com>
2019-10-29 20:51:40 -0400

    UI Changes

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -19,18 +19,53 @@ const StartGameScreen = props => {
   //     setEnteredGoal("");
   //   };
   return (
-    <View style={styles.screen1}>
-      <Text>Works!</Text>
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput placeholder="Number" />
+        <View style={styles.btn}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  screen1: {
+  screen: {
     paddingTop: 10,
     alignItems: "center"
     // flex: 1,
     //justifyContent: "center"
+  },
+  btn: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    margin: 15
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+    margin: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: "white",
+    padding: 20,
+    elevation: 5,
+    borderRadius: 10,
+    margin: 30,
+    
   }
 });
 
